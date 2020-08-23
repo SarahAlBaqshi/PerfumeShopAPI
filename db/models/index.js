5a5fbe14d6a769215b2a8f886ea7cb83ec3fbb6f
Sarah <S00038939@auk.edu.kw>
2020-08-23 18:48:43 +0300

    defined relatiionships between order and user

--- db/models/index.js
+++ db/models/index.js
@@ -1,6 +1,7 @@
 const Perfume = require("./Perfume");
 const Shop = require("./Shop");
 const User = require("./User");
+const Order = require("./Order");
 
 // a shop has many perfumes
 Shop.hasMany(Perfume, {
@@ -15,8 +16,13 @@ User.hasOne(Shop, { as: "shop", foreignKey: "userId" });
 
 Shop.belongsTo(User, { as: "user", foreignKey: "userId" });
 
+//one to many relationship between order and user
+User.hasMany(Order, { as: "orders", foreignKey: "userId" });
+Order.belongsTo(User, { as: "user" });
+
 module.exports = {
   Perfume,
   Shop,
   User,
+  Order,
 };
